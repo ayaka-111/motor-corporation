98e920d15d26a413ef542722323323a1c80b0d0b
ayaka <98952604+ayaka-111@users.noreply.github.com>
2022-03-27 11:57:10 +0900

    Update tab.js

--- tab.js
+++ tab.js
@@ -4,8 +4,8 @@ function GethashID (hashIDName){
     if(hashIDName){
       //タブ設定
       $('.tab li').find('a').each(function() { //タブ内のaタグ全てを取得
-        var idName = $(this).attr('href'); //タブ内のaタグのリンク名（例）#lunchの値を取得 
-        if(idName == hashIDName){ //リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
+        var idName = $(this).attr('href'); //タブ内のaタグのリンク名
+        if(idName == hashIDName){ //リンク元の指定されたURLのハッシュタグ
           var parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
           $('.tab li').removeClass("active"); //タブ内のliについているactiveクラスを取り除き
           $(parentElm).addClass("active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
